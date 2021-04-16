9521e1d5da40e3c382a05951fb0f23bc64535f54
周三甫 <215817301@qq.com>
2021-04-16 15:54:41 +0800

    20210416

--- classify_nsfw.py
+++ classify_nsfw.py
@@ -2,18 +2,17 @@
 import sys
 import argparse
 import tensorflow as tf
-
 from model import OpenNsfwModel, InputType
 from image_utils import create_tensorflow_image_loader
 from image_utils import create_yahoo_image_loader
-
 import numpy as np
+from get_urls import get_urls
+import pandas as pd
+import requests
 
 
 IMAGE_LOADER_TENSORFLOW = "tensorflow"
 IMAGE_LOADER_YAHOO = "yahoo"
-
-
 
 
 class NSFW:
@@ -22,39 +21,70 @@ class NSFW:
         self.fn_load_image = None
         self.sess = None
         self.model = OpenNsfwModel()
+        self.data_path = None
 
-    def session_run(self, input_file):
+    def session_run(self):
         with tf.compat.v1.Session() as self.sess:
             input_type = InputType[InputType.TENSOR.name.lower().upper()]
-            self.model.build(weights_path='data/open_nsfw-weights.npy', input_type=input_type)
+            self.model.build(
+                weights_path='data/open_nsfw-weights.npy', input_type=input_type)
 
             if input_type == InputType.TENSOR:
                 if IMAGE_LOADER_YAHOO == IMAGE_LOADER_TENSORFLOW:
-                    self.fn_load_image = create_tensorflow_image_loader(tf.Session(graph=tf.Graph()))
+                    self.fn_load_image = create_tensorflow_image_loader(
+                        tf.Session(graph=tf.Graph()))
                 else:
                     self.fn_load_image = create_yahoo_image_loader()
             elif input_type == InputType.BASE64_JPEG:
                 import base64
-                self.fn_load_image = lambda filename: np.array([base64.urlsafe_b64encode(open(filename, "rb").read())])
+                self.fn_load_image = lambda filename: np.array(
+                    [base64.urlsafe_b64encode(open(filename, "rb").read())])
 
             self.sess.run(tf.compat.v1.global_variables_initializer())
-            image = self.fn_load_image(input_file)
-            predictions = self.sess.run(self.model.predictions, feed_dict={self.model.input: image})
-            print(float(predictions[0][1]), type(float(predictions[0][1])))
-            if float(predictions[0][1]) >= 0.8 :
-                print("-------------")
+
+            # ======
+            data_txt = pd.read_table(self.data_path, sep='\t', header=None).values.tolist()
+            data_result = []
+            for d in data_txt:
+                urls = get_urls(str(d[0]))
+                print('网站 = ', str(d[0]), '图片数量:', len(urls))
+                if len(urls)>0:
+                    for url in urls:
+                        print(url)
+                        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'}
+                        try:
+                            res = requests.get(url, headers=headers, timeout=20)
+                        except:
+                            continue
+                        with open('img/text.jpg', 'wb') as f:
+                            f.write(res.content)
+                        f.close()
+
+                        try:
+                            image = self.fn_load_image('img/text.jpg')
+                        except:
+                            continue
+                        predictions = self.sess.run(self.model.predictions, feed_dict={self.model.input: image})
+                        # print(float(predictions[0][1]), type(float(predictions[0][1])))
+                        if float(predictions[0][1]) >= 0.8:
+                            print('图片 = ', url, '  结果：True', )
+                            data_result.append(str(d[0]) + '\tTrue')
+                            continue
+
+        return data_result
+
 
 
     def predict(self, input_file):
         image = self.fn_load_image(input_file)
-        predictions = self.sess.run(self.model.predictions, feed_dict={self.model.input: image})
+        predictions = self.sess.run(self.model.predictions, feed_dict={
+                                    self.model.input: image})
 
         #print("Results for '{}'".format(input_file))
         #print("\tSFW score:\t{}\n\tNSFW score:\t{}".format(*predictions[0]))
         print(float(predictions[0][1]), type(float(predictions[0][1])))
-        if float(predictions[0][1]) >= 0.8 :
+        if float(predictions[0][1]) >= 0.8:
             print("-------------")
-
 
 
 def main(input_file):
@@ -62,37 +92,44 @@ def main(input_file):
 
     with tf.compat.v1.Session() as sess:
         input_type = InputType[InputType.TENSOR.name.lower().upper()]
-        model.build(weights_path='data/open_nsfw-weights.npy', input_type=input_type)
+        model.build(weights_path='data/open_nsfw-weights.npy',
+                    input_type=input_type)
 
         self.fn_load_image = None
 
         if input_type == InputType.TENSOR:
             if IMAGE_LOADER_YAHOO == IMAGE_LOADER_TENSORFLOW:
-                self.fn_load_image = create_tensorflow_image_loader(tf.Session(graph=tf.Graph()))
+                self.fn_load_image = create_tensorflow_image_loader(
+                    tf.Session(graph=tf.Graph()))
             else:
                 self.fn_load_image = create_yahoo_image_loader()
         elif input_type == InputType.BASE64_JPEG:
             import base64
-            self.fn_load_image = lambda filename: np.array([base64.urlsafe_b64encode(open(filename, "rb").read())])
+            self.fn_load_image = lambda filename: np.array(
+                [base64.urlsafe_b64encode(open(filename, "rb").read())])
 
         sess.run(tf.compat.v1.global_variables_initializer())
         image = self.fn_load_image(input_file)
-        predictions = sess.run(model.predictions, feed_dict={model.input: image})
+        predictions = sess.run(model.predictions, feed_dict={
+                               model.input: image})
 
         #print("Results for '{}'".format(input_file))
         #print("\tSFW score:\t{}\n\tNSFW score:\t{}".format(*predictions[0]))
         print(float(predictions[0][1]), type(float(predictions[0][1])))
-        if float(predictions[0][1]) >= 0.8 :
+        if float(predictions[0][1]) >= 0.8:
             print("-------------")
-
-
 
 
 if __name__ == "__main__":
     # main(sys.argv)
     # main(r'C:\Users\Administrator\Documents\GitHub\tensorflow-open_nsfw\img\test2.jpg')
-
     nsfw = NSFW()
-    nsfw.session_run(r'C:\Users\Administrator\Documents\GitHub\tensorflow-open_nsfw\img\test2.jpg')
-    #nsfw.predict(r'C:\Users\Administrator\Documents\GitHub\tensorflow-open_nsfw\img\test2.jpg')
+    nsfw.data_path=r'C:\Users\Administrator\Documents\GitHub\tensorflow-open_nsfw\测试网站.txt'
+
+    data_result = nsfw.session_run()
+
+    with open('data_result.txt', 'wb') as f:
+        for i in data_result:
+            f.write(i + '\n')
+    f.close()
 
---
+++ get_urls.py
@@ -0,0 +1,57 @@
+import requests
+from lxml import etree
+import re
+import json
+import random
+
+
+def get_urls(url):
+    
+    headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
+    }
+    try:
+        req = requests.get(url, headers=headers, timeout=5)
+    except:
+        pass
+    # =====重定向跳转网址获取=====
+    # reditList = r.history#可以看出获取的是一个地址序列
+    # print(f'获取重定向的历史记录：{reditList}')
+    # print(f'获取第一次重定向的headers头部信息：{reditList[0].headers}')
+    # print(f'获取重定向最终的url：{reditList[len(reditList)-1].headers["location"]}')
+    listurls = []
+    try:
+        html = etree.HTML(req.text)
+        meta_urls = html.xpath('//meta/@content')
+        jump_urls = re.findall(r'http.+', meta_urls[len(meta_urls)-1])
+        if len(jump_urls) > 0:
+            website = re.findall(r'.*/', str(jump_urls[0]))[0]
+            req = requests.get(jump_urls[0])
+            if len(re.findall(r"(?=https.*).*jpg", req.text)) > 0:
+                listurls = re.findall(r"(?=https.*).*jpg", req.text)
+                listurls = random.sample(listurls, 3)
+            elif len(re.findall(r"/.*jpg", req.text)) > 0:
+                listurls = []
+                for i in re.findall(r"/.*jpg", req.text):
+                    listurls.append(website + str(i))
+                listurls = random.sample(listurls, 3)
+    except:
+        pass
+
+
+    try:
+        if len(re.findall(r"(?=https.*).*jpg", req.text)) > 0:
+            listurls = re.findall(r"(?=https.*).*jpg", req.text)
+            listurls = random.sample(listurls, 3)
+    except:
+        pass
+
+    return listurls
+
+
+urls = get_urls('https://cutt.ly/Qc8hmonJavob')
+for url in urls:
+    res = requests.get(url, timeout=5)
+    with open('img/text.jpg', 'wb') as f:
+        f.write(res.content)
+    f.close()
