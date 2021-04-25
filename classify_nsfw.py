f769b25ee9072304281719d19d17b0068eaaec63
周三甫 <215817301@qq.com>
2021-04-25 11:12:50 +0800

    Update classify_nsfw.py

--- classify_nsfw.py
+++ classify_nsfw.py
@@ -9,10 +9,16 @@ import numpy as np
 from get_urls import get_urls
 import pandas as pd
 import requests
+import prestodb
+import time
+import datetime
+import pymysql
+
 
 
 IMAGE_LOADER_TENSORFLOW = "tensorflow"
 IMAGE_LOADER_YAHOO = "yahoo"
+
 
 
 class NSFW:
@@ -23,7 +29,7 @@ class NSFW:
         self.model = OpenNsfwModel()
         self.data_path = None
 
-    def session_run(self):
+    def session_run(self, source_type):
         with tf.compat.v1.Session() as self.sess:
             input_type = InputType[InputType.TENSOR.name.lower().upper()]
             self.model.build(
@@ -43,16 +49,21 @@ class NSFW:
             self.sess.run(tf.compat.v1.global_variables_initializer())
 
             # ======
-            data_txt = pd.read_table(self.data_path, sep='\t', header=None).values.tolist()
+            data_txt = pd.read_table(
+                self.data_path, sep='\t', header=None).values.tolist()
             data_result = []
+
             for d in data_txt:
                 urls = get_urls(str(d[0]))
-                print('网站 = ', str(d[0]), '图片数量:', len(urls))
-                if len(urls)>0:
+                # print('网站 = ', str(d[0]), '图片数量:', len(urls))
+                if len(urls) > 0:
+                    treu_sum = 0
                     for url in urls:
-                        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'}
+                        headers = {
+                            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'}
                         try:
-                            res = requests.get(url, headers=headers, timeout=20)
+                            res = requests.get(
+                                url, headers=headers, timeout=20)
                         except:
                             continue
                         with open('img/text.jpg', 'wb') as f:
@@ -63,16 +74,23 @@ class NSFW:
                             image = self.fn_load_image('img/text.jpg')
                         except:
                             continue
-                        predictions = self.sess.run(self.model.predictions, feed_dict={self.model.input: image})
+                        predictions = self.sess.run(self.model.predictions, feed_dict={
+                                                    self.model.input: image})
                         # print(float(predictions[0][1]), type(float(predictions[0][1])))
                         if float(predictions[0][1]) >= 0.8:
-                            #print('------图片 = ', url, '  结果：True', )
-                            data_result.append(str(d[0]) + '\tTrue')
-                            continue
+                            treu_sum += 1
+
+                    if treu_sum / len(urls) >= 0.2:
+                        data_result.append(str(d[0]))
+                        sday = time.strftime(
+                            '%Y-%m-%d', time.localtime(time.time()))
+                        # print("INSERT INTO `pornographic_website_detection_zsf` (`sday`, `type`, `url`) VALUES ('" + sday + "', '"+source_type+"', '" + str(d[0]) + "' ,'" + str(d[1]) + "')")
+                        # mysql_insert("INSERT INTO `pornographic_website_detection_zsf` (`sday`, `type`, `url`, `sum`) VALUES ('" +
+                                        sday + "', '"+source_type+"', '" + str(d[0]) + "' ,'" + str(d[1]) + "')")
+            
+                    print('网站 = ', str(d[0]), '图片数量:', len(urls), '色情图片情况', treu_sum, len(urls) - treu_sum, '占比:', treu_sum / len(urls))
 
         return data_result
-
-
 
     def predict(self, input_file):
         image = self.fn_load_image(input_file)
@@ -86,48 +104,15 @@ class NSFW:
             print("-------------")
 
 
-def main(input_file):
-    model = OpenNsfwModel()
-
-    with tf.compat.v1.Session() as sess:
-        input_type = InputType[InputType.TENSOR.name.lower().upper()]
-        model.build(weights_path='data/open_nsfw-weights.npy',
-                    input_type=input_type)
-
-        self.fn_load_image = None
-
-        if input_type == InputType.TENSOR:
-            if IMAGE_LOADER_YAHOO == IMAGE_LOADER_TENSORFLOW:
-                self.fn_load_image = create_tensorflow_image_loader(
-                    tf.Session(graph=tf.Graph()))
-            else:
-                self.fn_load_image = create_yahoo_image_loader()
-        elif input_type == InputType.BASE64_JPEG:
-            import base64
-            self.fn_load_image = lambda filename: np.array(
-                [base64.urlsafe_b64encode(open(filename, "rb").read())])
-
-        sess.run(tf.compat.v1.global_variables_initializer())
-        image = self.fn_load_image(input_file)
-        predictions = sess.run(model.predictions, feed_dict={
-                               model.input: image})
-
-        #print("Results for '{}'".format(input_file))
-        #print("\tSFW score:\t{}\n\tNSFW score:\t{}".format(*predictions[0]))
-        print(float(predictions[0][1]), type(float(predictions[0][1])))
-        if float(predictions[0][1]) >= 0.8:
-            print("-------------")
-
-
 if __name__ == "__main__":
-    nsfw = NSFW()
-    nsfw.data_path=r'测试网站.txt'
+    data = []
 
-    data_result = nsfw.session_run()
-    print(data_result, type(data_result))
-
-    with open('data_result.txt', 'w') as result:
-        for i in data_result:
-            result.write(str(i) + '\n')
+    print('===================文本长度：', len(data), '; ', len(data[0]), data[0])
+    with open('测试网站.txt', 'w') as result:
+        for i in data:
+            result.write(str(i[0]) + '\t' + str(i[1]) + '\n')
     result.close()
 
+    nsfw = NSFW()
+    nsfw.data_path = '测试网站.txt'
+    data_result = nsfw.session_run('测试网站')
